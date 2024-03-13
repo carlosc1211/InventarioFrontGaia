e27f67f34b14bda9532b18ae7f49304af436a448
Carlos Carvajal <carvajalfiamengo1211@gmail.com>
2024-03-13 21:36:31 +0100

    Iniciamos

---
+++ src/app/Components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AuthService } from 'src/app/Service/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  formData = {
+    Usuario: '',
+    Contrasenya: ''
+  };
+
+  constructor(private authService: AuthService, private router:Router) {}
+
+  submitForm() {
+    const usuario = { Usuario: this.formData.Usuario, Contrasenya: this.formData.Contrasenya };
+
+    this.authService.login(usuario).subscribe(response => {
+      console.log(response.token);
+      if (response.token!='') {
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './Components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
