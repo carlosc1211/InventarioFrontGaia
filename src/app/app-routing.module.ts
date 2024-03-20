82bb0b6facfca9511e2d4a2bb58e1105e2ebbb51
Carlos Carvajal <carvajalfiamengo1211@gmail.com>
2024-03-20 23:46:52 +0100

    Modificamos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
-import { HomeComponent } from './Components/home/home.component';
 import { LaunchpadComponent } from './Components/launchpad/launchpad.component';
+import { InventarioComponent } from './Components/inventario/inventario.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'launchpad', component: LaunchpadComponent }
+  { path: 'launchpad', component: LaunchpadComponent },
+  { path: 'inventario', component: InventarioComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './Components/home/home.component';
 import { LaunchpadComponent } from './Components/launchpad/launchpad.component';
 import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { InventarioComponent } from './Components/inventario/inventario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
     LaunchpadComponent,
-    NavbarComponent
+    NavbarComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
