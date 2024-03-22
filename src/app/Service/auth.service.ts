c13913ae95fc5209df483073b2319a07e299cef4
Carlos Carvajal <carvajalfiamengo1211@gmail.com>
2024-03-22 20:54:02 +0100

    guardamos

--- src/app/Service/auth.service.ts
+++ src/app/Service/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class AuthService {
   private loginUrl = 'https://localhost:44357/api/Usuario/Login';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(user: any): Observable<any> {
     const headers = new HttpHeaders({
@@ -20,6 +21,10 @@ export class AuthService {
       tap((response) => console.log(response)),
       catchError(this.handleError<any>('login', [])),
     );
+  }
+
+  logout(): void {
+    this.router.navigate(['/login']);
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
